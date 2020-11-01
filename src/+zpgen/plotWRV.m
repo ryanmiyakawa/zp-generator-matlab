867614d390d58d5b918651b2173f398d2120704e
Ryan-Miyakawa <ryan@hooktheory.com>
2020-10-31 17:19:24 -0700

    new bin

--- src/+zpgen/plotWRV.m
+++ src/+zpgen/plotWRV.m
@@ -1,19 +1,24 @@
-function plotWRV
+function plotWRV(skip)
+
+if nargin == 0
+    skip = 1;
+end
 
 
 [d, p] = uigetfile('*.wrv');
 fid = fopen([p, d], 'r');
 
-
+idx = 0;
 while 1
+    idx = idx + 1;
+    if (skip > 1 && mod(idx, skip) ~= 0)
+        continue
+    end
+    
     tline = fgetl(fid);
     
     if ~ischar(tline), break, end
-    zpgen.patchTrapLine(tline);
-    
-    
-    
-%     disp(tline)
+    zpgen.patchTrapLine(tline);    
 end
 fclose(fid);
 
